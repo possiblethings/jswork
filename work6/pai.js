1863fdbd903466f97ccd1ffd41769ba3c1cb046b
your name <1934648830@qq.com>
2019-10-20 09:55:36 +0800

    修改

---
+++ work6/pai.js
@@ -0,0 +1,10 @@
+
+function calculate(){
+
+    let num =document.getElementById('pai').value 
+    num = parseInt(num)&&Number(num)
+    if(!isNaN(num)){
+
+        document,getElementById().value = pai()
+    }
+}
